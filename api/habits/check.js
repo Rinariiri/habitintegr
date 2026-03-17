599cd90d8cefcd68da715ebe569c49282fbed28d
Rinariiri <ahaydn@me.com>
2026-03-17 10:45:07 +0000

    Update check.js

--- api/habits/check.js
+++ api/habits/check.js
@@ -139,7 +139,7 @@ export default async function handler(req, res) {
             title: [
               {
                 text: {
-                  content: `${habitName} ${todayLabel}`
+                  content: `${habitName}`
                 }
               }
             ]
